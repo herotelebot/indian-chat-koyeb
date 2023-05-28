4eb8f377b4e2cf9e93819b8a86ef29c4c65df07d
herotelebot <52422404+herotelebot@users.noreply.github.com>
2023-05-28 17:47:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,128 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+socketio = SocketIO(app)
+
+rooms = {}
 
 
-if __name__ == "__main__":
-    app.run()
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+    return code
+
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/home", methods = ["GET", "POST"])
+def home():
+  #return render_template('contact.html')
+  return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return "Hello akhihhl"
+
+@app.route('/privacypolicy')
+def privacypolicy():
+    return render_template('privacypolicy.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route("/chatpage", methods = ["GET", "POST"])
+def chatpage():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("chatpage.html", error="Please enter a name.", code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("chatpage.html", error="Please enter a room code.", code=code, name=name)
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("chatpage.html", error="The room does not exist.", code=code, name=name)
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for('room'))
+
+    return render_template('chatpage.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+
+@socketio.on("connect")
+def connect(_):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f'{name} joined room {room}')
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f'{name} has left the room {room}')
+
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f'{session.get("name")} said {data["data"]}')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.host = '0.0.0.0'
+    #app.port = 8080
+    socketio.run(app,host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)
